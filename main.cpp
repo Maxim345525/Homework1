bf5deb191909dd286e57b06d1cab6db19b2e98d5
Maxim345525 <maximmaksimchyk@gmail.com>
2023-01-26 22:44:35 +0200

    add code

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a = 5;
+	cout << "Length (cm)-->" << a << endl;
+	int b = 7;
+	cout << "Width (cm)-->" << b << endl;
+	int c = 9;
+	cout << "Height (cm)-->" << c << endl;
+	int v;
+	v = a * b * c;
+	cout << "Volume:" << v << endl;
+	return 0;
+}
