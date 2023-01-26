b2b110e64edd8d105f41be40c5c2e4c986010f85
Maxim345525 <maximmaksimchyk@gmail.com>
2023-01-26 22:45:53 +0200

    add code

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int p = 3.14;
+	int R = 25;
+	cout << "Length (cm)-->" << R << endl;
+	int V;
+	V = 4 / 3 * p * R * 3;
+	cout << "Volume:" << V << endl;
+	return 0;
+}
